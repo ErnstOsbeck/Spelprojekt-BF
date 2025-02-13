4e009cfd1438c9c69af137b14999a935d1349b0d
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2025-02-13 14:07:44 +0100

    path

---
+++ SpelprojektBF/Spelprojekt BF/Assets/Scripts/Path.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] Points;
+    [SerializeField]
+    private float moveSpeed;
+    private int pointsIndex;
+
+    
+    void Start()
+    {
+       transform.position = Points[pointsIndex].transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pointsIndex <= pointsIndex - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+
+            if(transform.position == Points[pointsIndex].transform.position )
+            {
+                pointsIndex += 1;
+            }
+        }
+    }
+}
--- SpelprojektBF/Spelprojekt BF/Assets/Scripts/Player v.2.cs
+++ SpelprojektBF/Spelprojekt BF/Assets/Scripts/Player v.2.cs
@@ -1,29 +1,20 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Playerv2 : MonoBehaviour
 {
     public float speed = 1f;
     public float target = 10f;
     public GameObject UiObject;
     public int skräpTyp;
-    
+
     void Start()
     {
-        
+
     }
 
-    
     void Update()
     {
-        Vector3 move = new Vector3();
-
-        if (transform.position.x < target)
-        {
-            move.x = 1;
-        }
-        move.Normalize();
-        transform.position += move * Time.deltaTime * speed;
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
