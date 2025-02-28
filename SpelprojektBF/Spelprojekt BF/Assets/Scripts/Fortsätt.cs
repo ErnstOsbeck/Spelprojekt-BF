00a23b2e160644d8d618bb3412bf3457a5d90c4c
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2025-02-28 11:52:26 +0100

    l

--- SpelprojektBF/Spelprojekt BF/Assets/Scripts/Fortsätt.cs
+++ SpelprojektBF/Spelprojekt BF/Assets/Scripts/Fortsätt.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public class Fortsätt : MonoBehaviour
 {
+    public GameObject UiObject;
     public void OnClick()
     {
-        
         var path = GetComponent<Path>();
         path.enabled = true;
+        UiObject.SetActive(true);
     }
 }
