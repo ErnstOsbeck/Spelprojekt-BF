0319c87dc544832d1ebcbfae5dc3396d4ff063a5
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2025-03-04 11:01:27 +0100

    shrink

--- SpelprojektBF/Spelprojekt BF/Assets/Scripts/Player v.2.cs
+++ SpelprojektBF/Spelprojekt BF/Assets/Scripts/Player v.2.cs
@@ -7,16 +7,25 @@ public class Playerv2 : MonoBehaviour
     
     public int skräpTyp;
 
-    private int startposition;
+    private Vector3 startPosition;
+    private Vector3 currentPosition;
+    private Vector3 diff;
+    public float shrinkSpeed;
 
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     void Update()
     {
-        
+        currentPosition = transform.position;
+        diff = currentPosition - startPosition;
+        diff = diff.normalized;
+        diff.y = diff.x;
+        var path = GetComponent<Path>();
+        if (path.enabled == true)
+        transform.localScale -= diff * Time.deltaTime * shrinkSpeed;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
