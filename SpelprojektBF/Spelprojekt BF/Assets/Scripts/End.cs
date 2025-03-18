2d9d06ed8c4e4536e0a217825d1ae720acd2ea8e
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2025-03-18 11:08:31 +0100

    End PS

--- SpelprojektBF/Spelprojekt BF/Assets/Scripts/End.cs
+++ SpelprojektBF/Spelprojekt BF/Assets/Scripts/End.cs
@@ -2,10 +2,5 @@ using UnityEngine;
 
 public class End : MonoBehaviour
 {
-    public GameObject UI;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        UI.SetActive(true);
-        
-    }
+    
 }
